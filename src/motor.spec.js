9041a4897e5404a6b2b170c1d6b75fe5b629112c
Coral Cota Riera <ccota@intricom.es>
2023-11-17 07:14:40 +0100

    modulo 7-js

--- src/motor.spec.js
+++ src/motor.spec.js
@@ -1,12 +1,43 @@
 
 
 import { EL_NUMERO_ES_MAYOR, EL_NUMERO_ES_MENOR, ES_EL_NUMERO_SECRETO, NO_ES_UN_NUMERO, GAME_OVER_MAXIMO_INTENTOS } from "./modelo.js";
-import { comprobarNumero } from "./motor.js";
+import { comprobarNumero, generarNumeroAleatorio } from "./motor.js";
 
 import * as modelo from "./modelo.js";
+import { beforeEach} from "vitest";
 
+
+describe ("generarNumeroAleatorio", () => {
+    it ("MathRandom lo forzamos a que devuelva 0 y deberia de dar 0", () => {
+        //Arrange
+        const numeroEsperado = 0;
+        const spyOnMathRandom = vi.spyOn(global.Math, "random").mockReturnValue(0);
+
+        //Act
+        const resultado = generarNumeroAleatorio();
+
+        //Assert
+        expect(resultado).toBe(numeroEsperado)
+    });
+
+    it("MathRadom lo forzamos a 1 y deberia dar 100", () => {
+        //Arrange
+        const numeroEsperado = 100;
+        const spyOnMathRandom = vi.spyOn(global.Math, "random").mockReturnValue(0.999);
+
+        //Act
+        const resultado = generarNumeroAleatorio();
+
+        //Assert
+        expect(resultado).toBe(numeroEsperado)
+    });
+});
 
 describe ("comprobarNumero", () =>{
+    beforeEach (() => {
+        vi.spyOn(modelo, "numeroParaAcertar", "get").mockReturnValue(23);
+    });
+
     it("deberia de devolver NO_ES_UN_NUMERO cuando el texto no es un numero", ()=>{
         // Arrange
         const texto = "esto no es un numero";
@@ -19,9 +50,7 @@ describe ("comprobarNumero", () =>{
 
     it("Deberia de devolver ES_EL_NUMERO_SECRETO cuando texto es el numero a acertar", () => {
         //Arrange
-        const numeroParaAcertar = 23;
         const texto = "23";
-        vi.spyOn(modelo, numeroParaAcertar, "get").mockReturnValue(numeroParaAcertar);
 
         //Act
         const resultado = comprobarNumero(texto);
@@ -32,9 +61,8 @@ describe ("comprobarNumero", () =>{
 
     it("Deberia devolver EL_NUMERO_ES_MAYOR cuando el texto es un numero mayor al que tenemos que acertar", () =>{
         //Arrange
-        const numeroParaAcertar = 23;
         const texto = "24";
-        vi.spyOn(modelo, numeroParaAcertar, "get").mockReturnValue(numeroParaAcertar);
+        
         //Act
         const resultado = comprobarNumero(texto);
 
@@ -44,9 +72,7 @@ describe ("comprobarNumero", () =>{
 
     it("Deberia devolver EL_NUMERO_ES_MENOR cuando el texto es un numero menor al que tenemos que acertar", () =>{
         //Arrange
-        const numeroParaAcertar = 23;
         const texto = "22";
-        vi.spyOn(modelo, numeroParaAcertar, "get").mockReturnValue(numeroParaAcertar);
 
         //Act
         const resultado = comprobarNumero(texto);
@@ -57,9 +83,7 @@ describe ("comprobarNumero", () =>{
 
     it("Deberia de devolver GAME_OVER_MAXIMO_INTENTOS cuando se ha superado el maximo de intentos", () =>{
         //Arrange
-        const numeroParaAcertar = 23;
         const texto = 70;
-        vi.spyOn(modelo, numeroParaAcertar, "get").mockReturnValue(numeroParaAcertar);
         vi.spyOn(modelo, "numeroDeIntentos", "get" ).mockReturnValue(5);
 
         //Act
